44b62678423450b3e28cd15ca59395aea8e9f5e4
vivatm <vivatmarianne@gmail.com>
2017-05-19 23:20:01 +0200

    styleguide + reset

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var plugins = require('gulp-load-plugins')(); // tous les plugins de package.jso
 // Variables de chemins
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/style.scss')
+  return gulp.src('app/scss/main.scss')
     .pipe(plugins.sass()) // Converts Sass to CSS with gulp-sass
     .pipe(plugins.autoprefixer())
     .pipe(gulp.dest('app/css'))
