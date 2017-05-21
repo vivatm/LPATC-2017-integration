73f8650e5ec9fe4cd6833d46c182f43e0756b6ab
vivatm <vivatmarianne@gmail.com>
2017-05-21 20:27:12 +0200

    ajout header

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,10 @@ var plugins = require('gulp-load-plugins')(); // tous les plugins de package.jso
 gulp.task('sass', function(){
   return gulp.src('app/scss/main.scss')
     .pipe(plugins.sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(plugins.autoprefixer())
+    .pipe(plugins.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('app/css'))
 });
 
