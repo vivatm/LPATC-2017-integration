566e9fdd1de72b407038fa2bbaebc0ec275b585b
Marianne Vivat <mvivat@actency.local>
2017-07-17 14:52:52 +0200

    responsive - final commit

--- app/js/main.js
+++ app/js/main.js
@@ -13,8 +13,43 @@ $(document).ready(function(){
       }
   });
 
+  $('#header .menu .wrapper').on('click', function(e) {
+    e.stopPropagation();
+  });
+
   $("#header #search > a").on("click", function(e) {
       $("#header #search").toggleClass("open");
       e.preventDefault();
   });
+
+  // menu mobile
+
+  var navbar = $("#header .navbar");
+  var tablet = 768;
+
+  function buildMobileMenu(deviceWidth) {
+    if ($(document).width() < deviceWidth) {
+      $('.menu .wrapper .span_7').append(navbar);
+    }
+    else {
+      console.log('desktop');
+      $('.menu .wrapper .span_7 .navbar').insertAfter($('#header .logo'));
+    }
+  }
+
+  buildMobileMenu(tablet);
+
+  $(window).resize(function() {
+      buildMobileMenu(tablet);
+  });
+
+  $('.menu .wrapper div h4').click(function() {
+    if ($(document).width() < tablet) {
+      console.log('clic');
+      $(this).toggleClass('open');
+      $(this).find('.fa').toggleClass('fa-chevron-down');
+      $(this).find('.fa').toggleClass('fa-chevron-up');
+    }
+  });
+
 });
